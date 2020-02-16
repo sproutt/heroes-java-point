8a53a0969fafa72cbb0a4b12eb2bad3fc6347507
chanmiJu <wncksal13@gmial.com>
2020-02-16 13:12:03 +0900

    review fix

--- src/main/java/point/Point.java
+++ src/main/java/point/Point.java
@@ -1,42 +1,39 @@
 package point;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
 import java.lang.String;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Point {
-    private String input;
-    private List<String> strListXY;
-    private ArrayList<Integer> valueXY;  //좌표를 추출하는 결과값
+    private List<String> inputCoordinate;
+    private List<Integer> valueXY;
 
-    public void setting(String input){  //문자열에서 좌표값만 추출
-
-        input = input.substring(1,input.length()-1);
+    private void setInput(String input) {
+        input = input.substring(1, input.length() - 1);
         input = input.replaceAll(" ", "");
-
+        inputCoordinate = Arrays.asList(input.split("\\s*,\\s*"));
         valueXY = new ArrayList<>(2);
-        valueXY.add(0,0); valueXY.add(1,0); //초기값 0으로 세팅
 
-        strListXY = Arrays.asList(input.split("\\s*,\\s*"));
+        valueXY.add(0, 0);
+        valueXY.add(1, 0);
+
     }
 
-    public ArrayList<Integer> xyCoordinate(String input){
-
-        setting(input);
+    public List<Integer> extractXY(String input) {
         int value;
 
-        for (int i =0;i<strListXY.size()&&i<2;i++){   //길이 최대 2개
+        setInput(input);
 
-            if(!strListXY.get(i).equals("")) { //빈 값이 아닐때
-                value=Integer.parseInt(strListXY.get(i));
-                if(value<=0||value>=25) value=-1; // 범위를 넘어가면 -1을 넣어줌
-                valueXY.set(i,value);
+        for (int i = 0; i < inputCoordinate.size() && i < 2; i++) {
+            if (!inputCoordinate.get(i).equals("")) {
+                value = Integer.parseInt(inputCoordinate.get(i));
+                if (value <= 0 || value >= 25) {
+                    value = -1;
+                }
+                valueXY.set(i, value);
             }
         }
         return valueXY;
     }
-
 }
--- src/test/java/point/PointTest.java
+++ src/test/java/point/PointTest.java
@@ -3,58 +3,63 @@ package point;
 import org.junit.Before;
 import org.junit.Test;
 
-
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class PointTest {
-
     private Point point;
-    private ArrayList<Integer> result;
+    private List<Integer> result;
 
     @Before
-    public void setUp(){
-        point=new Point();
+    public void setUp() {
+        point = new Point();
         result = new ArrayList<>(2);
-        result.add(0,0); result.add(1,0);
+
+        result.add(0, 0);
+        result.add(1, 0);
     }
 
     @Test
-    public void testSingle() {
-        //값이 안들어간 좌표에는 0을 넣어준다.
-        result.set(0,12); result.set(1,0);
-        assertEquals(point.xyCoordinate("(12, )"),result);
+    public void 값이_안들어간_좌표에는_0을_넣어준다() {
+        result.set(0, 12);
+        result.set(1, 0);
+        assertEquals(point.extractXY("(12, )"), result);
 
-        result.set(0,0); result.set(1,14);
-        assertEquals(point.xyCoordinate("( ,14)"),result);
+        result.set(0, 0);
+        result.set(1, 14);
+        assertEquals(point.extractXY("( ,14)"), result);
     }
 
 
     @Test
-    public void testMore() {
-        //맨 처음 2개의 값만 추출한다.
-        result.set(0,12); result.set(1,11);
-        assertEquals(point.xyCoordinate("(12,11,10,9)"),result);
+    public void 맨_처음_2개의_값만_추출한다() {
+        result.set(0, 12);
+        result.set(1, 11);
+        assertEquals(point.extractXY("(12,11,10,9)"), result);
 
-        result.set(0,1); result.set(1,2);
-        assertEquals(point.xyCoordinate("(1,2,6)"),result);
+        result.set(0, 1);
+        result.set(1, 2);
+        assertEquals(point.extractXY("(1,2,6)"), result);
 
-        result.set(0,19); result.set(1,7);
-        assertEquals(point.xyCoordinate("(19,7)"),result);
+        result.set(0, 19);
+        result.set(1, 7);
+        assertEquals(point.extractXY("(19,7)"), result);
     }
 
     @Test
-    public void testInvalid(){
-        //유효하지 않는 좌표에는 -1를 넣어준다.
-        result.set(0,-1); result.set(1,-1);
-        assertEquals(point.xyCoordinate("(-46,123)"),result);
+    public void 유효하지_않는_좌표에는_minus_one를_넣어준다() {
+        result.set(0, -1);
+        result.set(1, -1);
+        assertEquals(point.extractXY("(-46,123)"), result);
 
-        result.set(0,-1); result.set(1,4);
-        assertEquals(point.xyCoordinate("(0, 4)"),result);
+        result.set(0, -1);
+        result.set(1, 4);
+        assertEquals(point.extractXY("(0, 4)"), result);
 
-        result.set(0,6); result.set(1,-1);
-        assertEquals(point.xyCoordinate("(6,25)"),result);
+        result.set(0, 6);
+        result.set(1, -1);
+        assertEquals(point.extractXY("(6,25)"), result);
     }
 }
